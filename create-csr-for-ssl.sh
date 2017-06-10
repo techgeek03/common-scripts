185b2ee1bb4d085faab2bb816c3552b8a799637c
techgeek <gordan.jandreoski@gmail.com>
2017-06-10 16:05:38 +0200

    Added script for converting .pem private key to .pfx

--- create-csr-for-ssl.sh
+++ create-csr-for-ssl.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 openssl genrsa -out fqdn.key 4096
+#You will need to provide the fqn of your domain and other attributes
 openssl req -out csr.csr -key fqdn.key -new -sha256
 openssl req -in csr.csr -noout -text
